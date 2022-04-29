54b921c2b615af59d6a035adb63f1836c4fbe012
nickfrosst <nickfrosst@gmail.com>
2022-04-29 00:55:26 +0000

    removed slack key

--- pirate_bot/slack_pirate.py
+++ pirate_bot/slack_pirate.py
@@ -2,10 +2,11 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
-SLACK_BOT_TOKEN = '46873ec258b13596fc2257133dd9fc0a'
-SLACK_APP_TOKEN = 'xapp-1-A03DTFAJH9P-3456932096339-9f15e0764ba8488089c545687e264a93cb41c8174cd5c552be468832765c5607'
+SLACK_BOT_TOKEN = sys.argv[1]
+SLACK_APP_TOKEN = sys.argv[0]
 
 app = App(token=SLACK_BOT_TOKEN)
 
