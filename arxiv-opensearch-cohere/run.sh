d92a6f2d720ec9b753df95f17f9322ce3824dad1
nabsabraham <nabila.abraham@gmail.com>
2023-05-03 10:03:28 -0400

    remove local run file

--- arxiv-opensearch-cohere/run.sh
+++
@@ -1,2 +0,0 @@
-source opensearchvenv/bin/activate
-streamlit run demoapp.py --browser.gatherUsageStats=False --server.port=8080 --server.address=$(tailscale ip) --logger.level=info 2> streamlit_logs.log
